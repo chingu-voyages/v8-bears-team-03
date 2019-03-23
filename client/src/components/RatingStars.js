c4df8b7223f2ced2d9e8de6183d5260fabf6d40a
John Meade <john.meade.webdev@gmail.com>
2019-03-22 20:50:19 -0600

    added keys in while loops

--- client/src/components/RatingStars.js
+++ client/src/components/RatingStars.js
@@ -2,15 +2,18 @@ import React from "react";
 
 function RatingStars(props) {
   let stars = [];
+  let starKey = 0;
   while (stars.length < props.rating) {
-    stars.push(<i className="fas fa-star" />);
+    stars.push(<i className="fas fa-star" key={starKey} />);
+    starKey += 1;
   }
   while (stars.length < 5) {
-    stars.push(<i className="far fa-star" />);
+    stars.push(<i className="far fa-star" key={starKey} />);
+    starKey += 1;
   }
 
   return (
-    <p className="feed-rating" key={props.name + props.rating}>
+    <p className="feed-rating" key={props.rating + 1}>
       {stars}
     </p>
   );
