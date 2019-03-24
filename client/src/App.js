289c8ab96afebb1bde9274ec3be7fc7d466ff794
pop <starrs33d@codelikeagrey.me>
2019-03-24 15:35:15 -0700

    set this up for use in the project

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,46 @@
-// import React, { Component } from "react";
-// import logo from "./logo.svg";
-// import "./App.css";
-// import FeedItemComp from "./components/FeedItemComp";
+import React, { useState, useEffect } from "react";
+import NavBar from "./components/NavBar";
+import DisplayCase from "./components/DisplayCase";
+import BeverageFeed from "./components/BeverageFeed";
+import "./App.css";
 
-// class DrinkScroll extends Component {
-//   render() {
-//     return <FeedItemComp />;
-//   }
-// }
+function App() {
+  const [drinks, setDrinks] = useState([]);
 
-// export default DrinkScroll;
+  useEffect(() => {
+    fetch("http://localhost:8000/drinks")
+      .then(results => results.json())
+      .then(data => {
+        setDrinks(data.drinks);
+      });
+  }, []);
 
-// NOT CURRENTLY IN USE
+  const [drink, setDrink] = useState({});
+
+  function setDrinkWrapper(drink) {
+    setDrink(drink);
+  }
+
+  return (
+    <div className="app-container">
+      <NavBar />
+      <main>
+        <section id="display-case">
+          <div id="DisplayCase">
+            {drink.name ? <DisplayCase data={drink} /> : null}
+          </div>
+        </section>
+
+        <section id="beverage-feed">
+          <div id="BeverageFeed">
+            {drinks.length > 0 ? (
+              <BeverageFeed data={drinks} setter={setDrinkWrapper} />
+            ) : null}
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
