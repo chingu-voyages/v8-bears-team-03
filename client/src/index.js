1091a521dfe2951ff7ef09109c42f0c177ee5bea
pop <starrs33d@codelikeagrey.me>
2019-03-24 09:26:57 -0700

    removing fetch request from index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,15 +5,7 @@ import * as serviceWorker from "./serviceWorker";
 import BeverageFeed from "./components/BeverageFeed";
 import NavBar from "./components/NavBar";
 
-fetch("http://localhost:8000/drinks")
-  .then(results => results.json())
-  .then(data => {
-    ReactDOM.render(
-      <BeverageFeed data={data.drinks} />,
-      document.getElementById("BeverageFeed")
-    );
-  });
-
+ReactDOM.render(<BeverageFeed />, document.getElementById("BeverageFeed"));
 ReactDOM.render(<NavBar />, document.getElementById("NavBar"));
 
 // If you want your app to work offline and load faster, you can change
