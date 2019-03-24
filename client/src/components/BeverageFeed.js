5bc685b7a593bf4635b07175005bf79d49cb0eef
pop <starrs33d@codelikeagrey.me>
2019-03-24 15:36:09 -0700

    removing state higher up the tree

--- client/src/components/BeverageFeed.js
+++ client/src/components/BeverageFeed.js
@@ -1,38 +1,17 @@
-import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
+import React from "react";
 import RatingStars from "./RatingStars";
-import DisplayCase from "./DisplayCase";
 
 function BeverageFeed(props) {
-  const [drinks, setDrinks] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:8000/drinks")
-      .then(results => results.json())
-      .then(data => {
-        setDrinks(data.drinks);
-      });
-  }, []);
-
-  const [drink, setDrink] = useState({});
-
-  if (drink.name) {
-    ReactDOM.render(
-      <DisplayCase data={drink} />,
-      document.getElementById("DisplayCase")
-    );
-  }
-
   return (
     <div className="feed-box">
       <div className="feed-scroll">
-        {drinks.map(entry => (
+        {props.data.map(entry => (
           <div className="feed-card" key={entry.name + entry._id}>
             <img
               className="feed-image"
               src={entry.image}
               alt={entry.name}
-              onClick={() => setDrink(entry)}
+              onClick={() => props.setter(entry)}
             />
             <div className="feed-info-box">
               <p className="feed-title">{entry.name}</p>
