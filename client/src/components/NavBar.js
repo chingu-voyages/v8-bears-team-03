3e702d7515f4bbad2ee0deacab37f9b9f21932c0
pop <starrs33d@codelikeagrey.me>
2019-03-24 15:36:36 -0700

    semantic html5 for the nav bar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -15,24 +15,26 @@ function NavBar() {
   }
 
   return (
-    <div>
-      <h1>devbev</h1>
-      <ul>
-        <li id="beer" onClick={() => sortDrinks("beer")}>
-          beer
-        </li>
-        <li id="tea" onClick={() => sortDrinks("tea")}>
-          tea
-        </li>
-        <li id="coffee" onClick={() => sortDrinks("coffee")}>
-          coffee
-        </li>
-        <li id="liquor" onClick={() => sortDrinks("liquor")}>
-          liquor
-        </li>
-        <li id="add">add my drink</li>
-      </ul>
-    </div>
+    <nav>
+      <div id="NavBar">
+        <h1>devbev</h1>
+        <ul>
+          <li id="beer" onClick={() => sortDrinks("beer")}>
+            beer
+          </li>
+          <li id="tea" onClick={() => sortDrinks("tea")}>
+            tea
+          </li>
+          <li id="coffee" onClick={() => sortDrinks("coffee")}>
+            coffee
+          </li>
+          <li id="liquor" onClick={() => sortDrinks("liquor")}>
+            liquor
+          </li>
+          <li id="add">add my drink</li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
