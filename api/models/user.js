8c254678b39d0d163a259be14cbaa67833c51d34
pop <starrs33d@codelikeagrey.me>
2019-03-29 01:06:58 -0700

    user model

---
+++ api/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  avatar: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  }
+});
+
+module.exports = User = mongoose.model("User", userSchema);
