fea5868c0104d21fc84642999b975f4c04e4e167
pop <starrs33d@codelikeagrey.me>
2019-03-29 13:15:46 -0700

    comming auth controller

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -8,8 +8,10 @@ const GITHUB_CLIENT_ID = global.gConfig.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = global.gConfig.GITHUB_CLIENT_SECRET;
 
 module.exports = auth = (req, res) => {
+  // access code from GITHUB
   const { code } = req.query;
 
+  // send code to receive access token from GitHub
   request
     .post("https://github.com/login/oauth/access_token")
     .send({
@@ -19,19 +21,25 @@ module.exports = auth = (req, res) => {
     })
     .set("Accept", "application/json")
     .then(result => {
+      // access token from GitHub
       const token = result.body.access_token;
 
+      // fetch the data about the user
       request
         .get("https://api.github.com/user")
         .set("Authorization", "token " + token)
         .then(userReq => {
+          // user data
           const user = userReq.body;
+
+          // store information about the user
           let newUser = new User({
             username: user.login,
             avatar: user.avatar_url,
             name: user.name
           });
 
+          // look to see if the user already exist
           User.find({ username: user.login }, (err, arr) => {
             if (err) {
               res.send(err);
@@ -42,6 +50,8 @@ module.exports = auth = (req, res) => {
             }
           });
 
+          // if the user does not exist
+          // create a new user and save to the databse
           newUser.save().then(
             user => {
               res.send(user);
