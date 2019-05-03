b3c1ce6e5e5221c7916c6cd2d69212f3a305fb52
John Meade <john.meade.webdev@gmail.com>
2019-05-03 09:44:18 -0600

    fix HTML typo

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,28 +3,27 @@ import React from "react";
 function NavBar(props) {
   return (
     <nav id="nav-bar">
-      <div>
-        <h1>devbev</h1>
-        <ul>
-          <li id="all" onClick={() => props.addFilter("none")}>
-            all
-          </li>
-          <li id="beer" onClick={() => props.addFilter("beer")}>
-            beer
-          </li>
-          <li id="tea" onClick={() => props.addFilter("tea")}>
-            tea
-          </li>
-          <li id="coffee" onClick={() => props.addFilter("coffee")}>
-            coffee
-          </li>
-          <li id="liquor" onClick={() => props.addFilter("liquor")}>
-            liquor
-          </li>
-          <li id="add" onClick={() => props.addDrinkForm()}>
-            add my drink
-          </li>
-          {/* <li>
+      <h1>devbev</h1>
+      <ul>
+        <li id="all" onClick={() => props.addFilter("none")}>
+          all
+        </li>
+        <li id="beer" onClick={() => props.addFilter("beer")}>
+          beer
+        </li>
+        <li id="tea" onClick={() => props.addFilter("tea")}>
+          tea
+        </li>
+        <li id="coffee" onClick={() => props.addFilter("coffee")}>
+          coffee
+        </li>
+        <li id="liquor" onClick={() => props.addFilter("liquor")}>
+          liquor
+        </li>
+        <li id="add" onClick={() => props.addDrinkForm()}>
+          add my drink
+        </li>
+        {/* <li>
             <a href="https://github.com/login/oauth/authorize?client_id=5391d9587090cbee0c38">
               login
             </a>
